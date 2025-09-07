a848bc97d2b831e472e067aea9052bfda5ac01c4
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-09-07 21:28:38 +0600

    2nd time update

---
+++ src/components/routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes= createBrowserRouter([
+{
+    path:'/',
+    element:<h1>hellow this is react js</h1>
+}
+])
+
+export default routes;
